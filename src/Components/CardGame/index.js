e3179004ae5794dff70136312ee7b9101269b5ad
0ataideJunior0 <ataide.junior.mg@gmail.com>
2024-09-25 15:04:45 -0300

    create component CardGame

---
+++ src/Components/CardGame/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function CardGame() {
+  return `
+    
+      <article class = "card-game" >
+    <img src = "images/LOGOS.svg" alt = "Minha Logo">  
+
+    </article>
+
+    `;
+}
+
+export default CardGame;
---
+++ src/Components/PlayerScore/index.js
@@ -0,0 +1,11 @@
+function PlayerScore() {
+  return `
+      
+        <article>
+  
+      </article>
+  
+      `;
+}
+
+export default PlayerScore;
