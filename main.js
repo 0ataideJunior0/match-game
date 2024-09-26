65da08fb582d5990446f7e32850b3fa19e18ba8c
0ataideJunior0 <ataide.junior.mg@gmail.com>
2024-09-26 11:21:29 -0300

    Create PlayerName Component without CSS

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 
-import CardHtml from "./src/Components/CardHtml";
+//import CardHtml from "./src/Components/CardHtml";
 import Boardgame from "./src/objects/BoardGame";
+import PlayerName from "./src/Components/PlayerName";
 
 const $root = document.querySelector("#root");
-const $htmlBoardGame = Boardgame(6);
-const $htmlcardHtml = CardHtml();
 
-$root.insertAdjacentHTML("afterbegin", $htmlBoardGame);
-//$root.insertAdjacentHTML("afterbegin", $htmlcardHtml);
+$root.insertAdjacentHTML(
+  "afterbegin",
+  `
+    ${PlayerName("Player1")}
+    ${PlayerName("Player2")}
+    ${Boardgame(6)}
+
+    `
+);
