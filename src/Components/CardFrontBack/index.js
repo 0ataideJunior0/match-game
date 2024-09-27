afce9cdc806f19e0b5e7ab730c476329bb03b552
0ataideJunior0 <ataide.junior.mg@gmail.com>
2024-09-27 14:32:43 -0300

    ✨ Add click event in CardFrontback component

--- src/Components/CardFrontBack/index.js
+++ src/Components/CardFrontBack/index.js
@@ -2,9 +2,28 @@ import CardGame from "../CardGame";
 import "./style.css";
 
 function CardFrontBack() {
+  //window é uma variavel global,
+  //quando para especificar você precisa passar uma classe
+  //no exemplowindow.CardFrontBack = {};
+
+  window.CardFrontBack = {};
+  window.CardFrontBack.HandleClick = (event) => {
+    const $origin = event.target;
+    const $CardFrontBack = $origin.closest(".card-sides");
+
+    // A função .toggle faz todo esse trabalho do if-else para
+    // verificar se existe uma classe dentro do elemento
+    // if ($CardFrontBack.classList.contains("-active")) {
+    //   $CardFrontBack.classList.remove("-active");
+    // } else {
+    //   $CardFrontBack.classList.add("-active");
+    // }
+    $CardFrontBack.classList.toggle("-active");
+  };
+
   return /*html*/ `
 
-<article class ="card-sides"> 
+<article class ="card-sides" onClick = "CardFrontBack.HandleClick(event)"> 
 <div class = "card -front">
     ${CardGame("LOGOS")}
     </div>
