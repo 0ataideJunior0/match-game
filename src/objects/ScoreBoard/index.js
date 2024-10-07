3077d92d46f2f12b02f0930c140c68f3552cd816
0ataideJunior0 <ataide.junior.mg@gmail.com>
2024-10-07 13:29:36 -0300

    ✨ Now you can select player with SelectArrow

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,20 +1,21 @@
 import PlayerName from "../../Components/PlayerName";
 import ScorePoint from "../../Components/ScorePoint";
+import SelectArrow from "../../Components/SelectArrow";
 import Versus from "../../Components/Versus";
 import "../ScoreBoard/style.css";
 
 function ScoreBoard() {
   return /*html*/ `
    <header class = "score-board">
+   ${SelectArrow()}
+   </header> 
+   <article class= "body-ScoreBoard">
    ${PlayerName("Player1")}
    ${ScorePoint()}
    ${Versus("VS")}
-   ${PlayerName("Player2")}
    ${ScorePoint()}
-  
-
-   </header> 
-    
+   ${PlayerName("Player2")}
+   </article>
     `;
 }
 export default ScoreBoard;
