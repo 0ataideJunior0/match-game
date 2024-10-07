1a49384c7a4d004452557599d90df8a61748820e
0ataideJunior0 <ataide.junior.mg@gmail.com>
2024-10-07 13:27:41 -0300

    💄 Add new component SelecArrow

---
+++ src/Components/SelectArrow/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function SelectArrow(){
+ return /*html*/ `
+    <article>
+      <img src = "./images/selector.svg"    />
+    
+    </article>
+ 
+ 
+ `
+
+}
+export default SelectArrow;
