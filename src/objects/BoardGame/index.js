afcbbbceba1c18b2dd2ed261177d7121b5ffb8ee
0ataideJunior0 <ataide.junior.mg@gmail.com>
2024-10-08 13:32:27 -0300

    ✨ Cards flip when 2 cards is select

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,12 +3,21 @@ import cards from "./cards";
 import "./style.css";
 
 function Boardgame(amountCards) {
-
+  window.boardgame = {};
+  window.boardgame.handleClick = () =>{
+    const $boardGame = document.querySelector(".board-game");
+    const $cardActive = $boardGame.querySelectorAll(".card-sides.-active");
+    if ($cardActive.length === 2){
+    setTimeout(()=> {
+      $cardActive.forEach((card) => card.classList.remove("-active"))
+      }, 1000);
+    }
+  }
   const htmlCardList = cards.map( (card) => CardFrontBack(card.icon, card.iconAlt ) );
   const $htmlCardList = htmlCardList.join("");
 
   return /*html*/ `
-  <section class = "board-game">
+  <section class = "board-game" onClick="boardgame.handleClick()">
   ${$htmlCardList}
     </section>
   
